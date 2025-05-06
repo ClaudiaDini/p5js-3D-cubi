6d39bd46caf94a2314d6bb2602bbc69161534550
ClaudiaDini <cdini9325@gmail.com>
2025-05-06 13:08:29 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,43 @@
+let cubi = [];
+let copie = 30;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, "webgl");
+
+  for (let i = 0; i < copie; i++) {
+    let cubo = {
+      x: random(-1000, 1000),
+      y: random(-1000, 1000),
+      z: random(-1000, 1000),
+      size: 100,
+      color: random(["pink", "blue", "red"]),
+      //rotationAxis : random (["x", "y", "z"]),
+      rotationFunction: random([rotateX, rotateY, rotateZ]),
+    };
+    cubi.push(cubo);
+  }
 }
 
 function draw() {
-  background(220);
-  const s = 100;
+  background(0);
+  orbitControl();
 
-  rect(0, 0, s, s);
-  rect(0, height - s, s, s);
-  rect(width - s, 0, s, s);
-  rect(width - s, height - s, s, s);
+  for (let cubo of cubi) {
+    push();
+    translate(cubo.x, cubo.y, cubo.z);
+    cubo.rotationFunction(frameCount / 10);
+    //if (cubo.rotationAxis == "x") {
+    //rotateX(frameCount / 10);
+    //} else if (cubo.rotationAxis == "y") {
+    //  rotateY(frameCount / 20);
+    //} else (cubo.rotationAxis == "z") {
+    //  rotateZ(frameCount * 10);
+    //}
+
+    fill(cubo.color);
+    box(cubo.size);
+    pop();
+  }
 }
 
 function windowResized() {
